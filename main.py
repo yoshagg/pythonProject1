805f36f0d9b197616aac9cceaed361bb8061b967
yoshagg <smertinkirill@gmail.com>
2023-12-03 17:24:38 +0300

    second updates

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import utils
+from utils import get_data, get_filtered_data, get_formatted_data, get_last_values, print_operations
+
+def run():
+    d = get_data()
+    d = get_filtered_data(d)
+    d = get_last_values(d, 5)
+    readyline = "".join(print_operations(d))
+    print(readyline)
+
+if __name__ == "__main__":
+    run()
+
+### second commit
--- utils.py
+++ utils.py
@@ -1,21 +1,40 @@
-import json, datetime
+import json
+from datetime import datetime
 
-def get_data(data):
-    with open (f"{data}", "r", encoding="utf-8") as file:
-        d = json.load(file)
-    return d
+
+def get_data():
+    with open("operations.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
 
 def get_filtered_data(data):
     data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
     return data
 
-def get_last_values(data, last_values_count):
-    data = sorted(data, lambda x: x["data"], reversed=True)
-    data = data[:last_values_count]
-    return data
 
-def get_formatted_data(data):
-    data_list = []
-    date = datetime.strptime(row['date']), __format '%Y-%m-%dT%H:%M-%S%f')
+def get_last_values(data, last_values_count=5):
+    data = sorted(data, key=lambda x: x["date"], reverse=True)
+    return data[:last_values_count]
 
-#print(get_data("operations.json"))
+
+def get_formatted_data(date: str):
+    return datetime.strftime(date,'%d.%m.%Y')
+
+
+def hide_card_number(numbers, to=False):
+    if not to:
+        return f"{numbers[:4]} {numbers[4:6]}** **** {numbers[-4:]}"
+    else:
+        return f"** {numbers[-4:]}"
+
+
+def print_operations(data):
+    operations = []
+    for operation in data:
+        if "from" in operation:
+            date = get_formatted_data(operation['date'])
+            operations.append(f"""{date} {operation['description']} \n {hide_card_number(operation["from"])} \n"""
+                              f""" -> {hide_card_number(operation['to'])}\n\n""")
+        else:
+            pass
+    return operations
